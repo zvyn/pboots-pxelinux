6c6b5de2f23a217e25993abb5de2118317e78819
Milan Oberkirch <milan@oberkirch.org>
2014-02-14 17:54:32 +0100

    fixed ip config for linux clients

--- models.py
+++ models.py
@@ -57,7 +57,7 @@ class Item(models.Model):
                 lines.append('append %s' % self.append)
             if len(self.password):
                 lines.append('menu passwd %s' % self.password)
-            lines.append('ipappend 1')
+            lines.append('ipappend 3')
         return "\n".join(lines)
 
 
